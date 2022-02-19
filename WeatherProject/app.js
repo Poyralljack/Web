d68f2f44e3c21c709a3e21366f415a86619f26dd
Yasin Herken <yasin.herken123@gmail.com>
2022-02-19 18:57:39 +0300

    adding city in weather

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,21 +1,37 @@
 const express=require("express");
 
-const app=express();
-
 const https=require("https");
 
+const bodyParser=require("body-parser");
+
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/",function(req,res){
-  const url="https://api.openweathermap.org/data/2.5/weather?q=ankara&appid=3f3caad473759e23a80e32f63463b8c0&units=metric";
+  res.sendFile(__dirname+"/index.html");
+});
+app.post("/",function(req,res){
+  const query=req.body.cityName;
+  const apiKey="3f3caad473759e23a80e32f63463b8c0";
+  const unit="metric";
+  const url="https://api.openweathermap.org/data/2.5/weather?q="
+  +query+"&appid="+apiKey+"&units="+unit;
   https.get(url,function(response){
     console.log(response.statusCode);
     response.on("data",function(data){
       const weatherData=JSON.parse(data);
       const temp=weatherData.main.temp;
-      console.log(temp);
+      const description=weatherData.weather[0].description;
+      const icon=weatherData.weather[0].icon;
+      const imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png";
+      res.write("<p>The weather is currently "+description+"</p>");
+      res.write("<h1>The tempreture in "+query+" is "+temp+"degress Celcius.</h1>");
+      res.write("<img src="+imageURL+">");
+      res.send();
     });
   });
-  res.send("Server is up and running");
-});
+})
 
 app.listen(3000,function(){
   console.log("Server is running on 3000");
