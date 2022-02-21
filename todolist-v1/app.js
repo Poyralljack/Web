1602f36eb10b05384b8b5aeacd93950afcb6b767
Yasin Herken <yasin.herken123@gmail.com>
2022-02-21 17:41:24 +0300

    To do list template

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,11 +4,36 @@ const bodyParser=require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(express.static('public'))
+
+let items=["Buy Food","Cook Food","Eat Food"];
 app.get("/",function(req,res){
 
-  res.sendFile(__dirname+"/index.html");
+    let today = new Date();
+    let options = { 
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric' 
+    };
+    let day=today.toLocaleDateString("en-US",options);
+    res.render('list', {
+        kindOfDay: day,
+        newListItems:items
+    });
 
 })
 
+app.post("/",function(req,res){
+    let item =req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
+app.listen(3000,function(){
+    console.log("Server started on port 3000");
+    
+})
