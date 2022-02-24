13b3f38c1fd1ebb11d324a8856e0c99da29f154a
Yasin Herken <yasin.herken123@gmail.com>
2022-02-24 17:32:52 +0300

    relational database example on mongodb

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -5,20 +5,70 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/person",{useNewUrlParser:true});
 }
-
+const fruitSchema=new mongoose.Schema({
+  name: {
+    type:String,
+    required:[true,"Please check your data entry,no name specified"]
+  },
+  rating:{
+    type: Number,
+    min:1,
+    max:10
+  },
+  review:String
+});
 const personSchema=new mongoose.Schema({
     name: String,
-    age: Number
+    age: Number,
+    favouriteFruit:fruitSchema
 });
+const Fruit =mongoose.model("Fruit",fruitSchema);
 
 const Person =mongoose.model("Person",personSchema);
+const pineapple=new Fruit({
+  name:"Pineapple",
+  rating:9,
+  review:"Great fruit."
+});
+const person=new Person({
+  name:"Amy",
+  age:12,
+  favouriteFruit:pineapple
+})
+pineapple.save(function(err){
+  if(err)
+    return handleError(err); 
+});
 
-
-const person =new Person
-(
-    {
-    name:"John",
-    age:37
-    }
-);
 person.save();
+
+Person.find(function(err,people){
+  if(err){
+    console.log(err);
+  }else{
+    mongoose.connection.close();
+    people.forEach(function(person){
+      console.log(person.name);
+    });
+  }
+})
+
+/*Fruit.updateOne({_id:"62174aa01192db5067c70c57"}, {name: "Peach!"},function(err){
+  if(err){
+    console.log(err);
+    console.log("here");
+  }
+  else{
+    console.log("Succesfully added");
+  }
+});
+*/
+
+/*Person.deleteMany({name:'Amy'},function(err){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log("Succesfully deleted");
+  }
+})*/
