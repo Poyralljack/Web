daeeafd2cdfdff2dc4c7874262d130e9704af862
Yasin Herken <yasin.herken123@gmail.com>
2022-02-27 21:43:03 +0300

    adding md5 encryption

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,12 +1,11 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express= require("express");
-
 const bodyParser=require("body-parser");
-
 const ejs=require("ejs");
-
 const mongoose=require("mongoose"); 
+const md5=require('md5');
 
 const app = express();
 
@@ -18,10 +17,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 
-const userSchema={
+const userSchema=new mongoose.Schema({
     email:String,
     password:String
-}
+});
+
 
 const User=new mongoose.model("User",userSchema);
 
@@ -40,7 +40,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -53,7 +53,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
     User.findOne({email:username},function(err,foundUser){
         if(err){
             console.log(err);
