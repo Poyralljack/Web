33105696fdcd14742baa97bdedf510a905da15d6
Yasin Herken <yasin.herken123@gmail.com>
2022-03-02 15:46:48 +0300

    Secret submit and show it , last update

--- Secrets/app.js
+++ Secrets/app.js
@@ -30,7 +30,9 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 const userSchema=new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId:String,
+    secret: [{type:String}]
 });
 userSchema.plugin(findOrCreate);
 userSchema.plugin(passportLocalMongoose);
@@ -54,8 +56,6 @@ passport.serializeUser(function(user, done) {
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        console.log(user);
-        console.log(profile);
       return cb(err, user);
     });
   }
@@ -82,13 +82,39 @@ app.get("/register",function(req,res){
 });
 
 app.get("/secrets",function(req,res){
+    User.find({"secret":{$ne:null}},function(err,foundUsers){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+                res.render("secrets",{userWithSecrets:foundUsers});
+            }
+        }
+    })
+});
+app.get("/submit",function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     }else{
         res.redirect("/login");
     }
 });
-
+app.post("/submit",function(req,res){
+   const submittedSecret=req.body.secret; 
+   console.log(req.user._id);
+   User.findById(req.user._id,function(err,foundUser){
+       if(err){
+           console.log(err);
+       }else{
+           if(foundUser){
+               foundUser.secret.push(submittedSecret);
+               foundUser.save(function(){
+                   res.redirect("/secrets");
+               })
+           }
+       }
+   })
+});
 app.get("/logout",function(req,res){
     req.logOut();
     res.redirect("/");
